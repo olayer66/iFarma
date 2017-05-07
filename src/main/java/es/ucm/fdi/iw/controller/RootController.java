fd72cfc24a882fd7747f32c00333fc6e95768ff8
joset <jossan01@ucm.es>
2017-05-08 01:20:37 +0200

    Arreglado problema de mostrar nombre del usuario

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -2,22 +2,16 @@ package es.ucm.fdi.iw.controller;
 
 import java.security.Principal;
 
-import javax.management.relation.Role;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller	
 public class RootController {
@@ -33,7 +27,6 @@ public class RootController {
 	}
 	@RequestMapping("/login")
 	String login(HttpSession sesion, Principal principal) {
-		sesion.setAttribute("usuario",getUsuario());
 		return "/login";
 	}
 
@@ -67,17 +60,6 @@ public class RootController {
 		log.warn("Acceso no permitido");
 		return "/denegado";
 	}
-	private String getUsuario(){
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
- 
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails)principal).getUsername();
-        } else {
-            userName = principal.toString();
-        }
-        return userName;
-    }
 	private String getRole(){
 		String role;
         Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
