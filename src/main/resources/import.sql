091013b90a21411c9161391521f693a58f9b58d5
joset <jossan01@ucm.es>
2017-06-02 17:57:41 +0200

    caza el bug

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,14 @@
 INSERT INTO USUARIO VALUES(4,'hernandez','$2a$10$z4Wb59D8Nv57oHUhS2/zludDqaBOYIJ.W2sbrOHMP.RZ0w93RQ78e','hoeli@gmail.com',0,'antoniio','FAR','64266666','farma1')
 INSERT INTO USUARIO VALUES(2, 'gordo porras', '$2a$04$Uqd2b/WV0DHif1RdiXSPqeTkT79pT6ezLSQ6JJ6zosk0OXDwm9jjC', 'paco@algo.com',0, 'paco', 'MED', '91685336', 'medico')
 INSERT INTO USUARIO VALUES(1, 'gordo porras1', '$2a$04$Uqd2b/WV0DHif1RdiXSPqeTkT79pT6ezLSQ6JJ6zosk0OXDwm9jjC', 'paco@algo.com1',0, 'paco1', 'MED', '916853361', 'medico1')
+INSERT INTO USUARIO VALUES(5, 'Gordo Porras', '$2a$04$Uqd2b/WV0DHif1RdiXSPqeTkT79pT6ezLSQ6JJ6zosk0OXDwm9jjC', 'paco@algo.com',1, 'Paco', 'PAC', '91685336', 'paciente')
 
 INSERT INTO MEDICO VALUES ( 'C.S La alameda', '12/12/200057', 2)
 INSERT INTO MEDICO VALUES ( 'C.S La alameda', '12/12/200057', 1)
 
-INSERT INTO FARMACEUTICO VALUES ( '12/15/20007', 4)
+--INSERT INTO FARMACEUTICO VALUES ( '12/15/20007', 4)
 
+INSERT INTO PACIENTE VALUES ( 'Coslada', '28822',324, '011452247', 'Madrid', 'Calle falsa 1234', '05/19', 0, 47558226334,'Madrid', 5, 2)
 
 -- id_farmacia bigint, ciudad varchar(255) not null, cod_postal varchar(255) not null, 
 -- com_autonoma varchar(255), direccion varchar(255) not null, estado integer not null, nombre varchar(255) not null, provincia varchar(255) not null, telefono varchar(255) not null, duenio_id bigint, primary key (id_farmacia))
