9d19bf0bfa11474922ba92e49adf5a96f8f60d00
David Labrador <davlabrador@gmail.com>
2017-06-08 20:15:13 +0200

    Refactorizado metodo

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,6 +14,38 @@ $(function () {
         history.back()
     });
 
+    $('#comboComunidades').on('change', function () {
+      var $this = $(this);
+      var $provincia = $('#comboProvincia');
+      var comunidades = {
+        "Andalucia": ["Almería", "Granada","Córdoba","Jaén","Sevilla","Málaga","Cádiz","Huelva"],
+        "Aragon": ["Huesca","Zaragoza", "Teruel"],
+        "Principado de Asturias": ["Asturias"],
+        "Islas Baleares": ["Baleares"],
+        "Canarias": ["Santa Cruz de Tenerife","Las Palmas"],
+        "Cantabria": ["Santander"],
+        "Castilla-La Mancha": ["Toledo"," Ciudad Real"," Cuenca"," Guadalajara "," Albacete"],
+        "Castilla y Leon": ["León","Palencia","Salamanca","Burgos","Zamora","Valladolid","Soria","Segovia","Ávila"],
+        "Catalunya": ["Barcelona","Tarragona","Lérida ","Gerona"],
+        "Extremadura": ["Cáceres ","Badajoz"],
+        "Galicia": ["La Coruña"," Lugo"," Orense ","Pontevedra"],
+        "La Rioja": ["La Rioja"],
+        "Comunidad de Madrid": ["Madrid"],
+        "Navarra": ["Navarra"],
+        "Pais Vasco": ["Álava"," Guipúzcoa ","Vizcaya"],
+        "Region de Murcia": ["Murcia"],
+        "Comunidad Valenciana": ["Castellón"," Valencia ","Alicante"],
+        "Ceuta": ["Ceuta"],
+        "Melilla": ["Melilla"]
+      };
+
+      $provincia.html('');
+
+      $.each(comunidades[$this.val()], function () {
+        $provincia.append('<option value="' + this + '">' + this + '</option>');
+      });
+    });
+
     $("#pagoEfectivo").hide();
     $("#pagoTarjeta").hide();
     $("#pagoPayPal").hide();
@@ -110,149 +142,4 @@ $(function () {
           "info": true,
           "autoWidth": false
         });
-      $('#comboComunidades').on('change', function (e) {
-          var andalucia =["Almería", "Granada","Córdoba","Jaén","Sevilla","Málaga","Cádiz","Huelva"];
-          var aragon=["Huesca","Zaragoza", "Teruel"];
-          var asturias=["Asturias"];
-          var baleares=["Baleares"];
-          var canarias=["Santa Cruz de Tenerife","Las Palmas"];
-          var cantabria=["Santander"];
-          var castillaMancha=["Toledo"," Ciudad Real"," Cuenca"," Guadalajara "," Albacete"];
-          var castillaLeon=["León","Palencia","Salamanca","Burgos","Zamora","Valladolid","Soria","Segovia","Ávila"];
-          var catalunya=["Barcelona","Tarragona","Lérida ","Gerona"];
-          var extremadura=["Cáceres ","Badajoz"];
-          var galicia=["La Coruña"," Lugo"," Orense ","Pontevedra"];
-          var rioja=["La Rioja"];
-          var madrid=["Madrid"];
-          var navarra=["Navarra"];
-          var paisVasco=["Álava"," Guipúzcoa ","Vizcaya"];
-          var murcia=["Murcia"];
-          var valencia=["Castellón"," Valencia ","Alicante"];
-          var ceuta=["Ceuta"];
-          var melilla=["Melilla"];
-          var optionSelected = $("option:selected", this);
-          var valueSelected = this.value;
-          var select = document.getElementById('comboProvincia');
-          while (select.firstChild) {
-                select.removeChild(select.firstChild);
-            }
-          switch (valueSelected) {
-            case "Andalucia":
-                var myobject=andalucia;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Aragon":
-                var myobject=aragon;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Canarias":
-                var myobject=canarias;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Cantabria":
-                var myobject=cantabria;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Castilla y Leon":
-                var myobject=castillaLeon;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Castilla-La Mancha":
-                var myobject=castillaMancha;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Catalunya":
-                var myobject=catalunya;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Ceuta":
-                var myobject=ceuta;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Comunidad Valenciana":
-                var myobject=valencia;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Comunidad de Madrid":
-                var myobject=madrid;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Extremadura":
-                var myobject=extremadura;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Galicia":
-                var myobject=galicia;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Islas Baleares":
-                var myobject=baleares;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "La Rioja":
-                var myobject=rioja;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Melilla":
-                var myobject=melilla;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Navarra":
-                var myobject=navarra;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Pais Vasco":
-                var myobject=paisVasco;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Principado de Asturias":
-                var myobject=asturias;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            case "Region de Murcia":
-                var myobject=murcia;
-                for(index in myobject) {
-                    select.options[select.options.length] = new Option(myobject[index], myobject[index]);
-                }
-                break;
-            default:
-                break;
-        }
-    });
 });
